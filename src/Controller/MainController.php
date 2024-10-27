4d181b2ed8af607cb6227bdb9f5c54c14df88c06
ANSHAD-N <anshadnoushad12@gmail.com>
2024-10-27 22:05:04 +0530

    twig template engine added

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function homepage(): Response
+    public function homepage(): \Symfony\Component\HttpFoundation\Response
     {
-        return new Response('Hai new home page');
+        $starshipCount = 567;
+        $myShip = [
+            'name' => 'Star',
+            'Mode' => 'LM_06_XC',
+            'Captain' => 'Rofter Woolf',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'myShip' => $myShip,
+            'starshipCount' => $starshipCount,
+        ]);
     }
 }
