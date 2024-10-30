d66d1874728863cefb7f684261238dfea6299a2d
ANSHAD-N <anshadnoushad12@gmail.com>
2024-10-30 23:20:48 +0530

    new twig created and controller update to starship repository

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,24 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function homepage(): \Symfony\Component\HttpFoundation\Response
+    public function homepage(StarshipRepository $repository): \Symfony\Component\HttpFoundation\Response
     {
-        $starshipCount = 567;
-        $myShip = [
-            'name' => 'Star',
-            'Mode' => 'LM_06_XC',
-            'Captain' => 'Rofter Woolf',
-        ];
+        $myShip = $repository->findAll();
 
         return $this->render('main/homepage.html.twig', [
             'myShip' => $myShip,
-            'starshipCount' => $starshipCount,
+            'starshipCount' => count($myShip),
         ]);
+    }
+
+    #[Route('/starship/{id<\d+>}', name: 'starship_show')]
+    public function getStarshipDetail(int $id, StarshipRepository $repository)
+    {
+        $starship = $repository->findOne($id);
+
+        return $this->render('main/starship.html.twig', ['starship' => $starship]);
     }
 }
--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Models\Starship;
+use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,28 +10,22 @@ use Symfony\Component\Routing\Attribute\Route;
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships', name: 'api_starships')]
-    public function getCollections(): JsonResponse
+    public function getCollections(StarshipRepository $repository): JsonResponse
     {
-        $collections = [
-            new Starship(
-                'Star',
-                'LM_06_XC',
-                'Rofter Woolf',
-                'Under taken by JK Rowling',),
-            new Starship(
-                'Star',
-                'LM_06_XC',
-                'Rofter Woolf',
-                'Under taken by JK Rowling',),
-            new Starship(
-                'Star',
-                'LM_06_XC',
-                'Rofter Woolf',
-                'Under taken by JK Rowling',),
-        ];
+        $collections = $repository->findAll();
 
         return $this->json($collections);
-
     }
 
+    #[Route('/api/starships/{id<\d+>}', methods: ['GET'])]
+    public function getOne(int $id, StarshipRepository $repository): JsonResponse
+    {
+        $starship = $repository->findOne($id);
+
+        if (!$starship) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->json($starship);
+    }
 }
